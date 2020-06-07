76b3ea88a37eab10fa5e298f901796b378b71571
dchristofolli <danielchristofolli@gmail.com>
2020-06-07 18:42:21 -0300

    Criado consumer

---
+++ src/main/java/com/sicredi/cooperativeassembly/kafka/MessageConsumer.java
@@ -0,0 +1,19 @@
+package com.sicredi.cooperativeassembly.kafka;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.concurrent.CountDownLatch;
+
+@Slf4j
+@Data
+public class MessageConsumer {
+    private final CountDownLatch countDownLatch= new CountDownLatch(1);
+
+    @KafkaListener(topics = "${session-result.kafka.topic}", groupId = "user")
+    public void consumer(String payload){
+        log.info("Received voting session result: {}", payload);
+        countDownLatch.countDown();
+    }
+}
---
+++ src/main/java/com/sicredi/cooperativeassembly/kafka/configuration/ConsumerConfiguration.java
@@ -0,0 +1,56 @@
+package com.sicredi.cooperativeassembly.kafka.configuration;
+
+import com.sicredi.cooperativeassembly.kafka.MessageConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ConsumerConfiguration {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public Map<String, Object> consumerConfigs() {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "Kafka");
+        return properties;
+    }
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs(),
+                new StringDeserializer(),
+                new JsonDeserializer<>(String.class));
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
+    kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public MessageConsumer consumer() {
+        return new MessageConsumer();
+    }
+}
